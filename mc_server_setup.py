2afe819a6c65dfd692dbfbdf8b284c6e47d0221c
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2022-09-02 14:52:34 +0100

    Work on bedrock-paper loader type

--- mc_server_setup.py
+++ mc_server_setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 modules = ["base64", "requests", "json", "os"]
-version = "1.0.0"
+version = "1.1.0"
 
 def exitFunc():
 	input("> Press enter to exit <")
@@ -32,13 +32,29 @@ else:
 	try:
 		# TODO special versions (eg. bedrock-paper which has pre-installed addons defined but not accessible)
 		print("------")
-		typ = input("Specify a loader type. (vanilla | paper | fabric) >> ")
+		# Loader types: VANILLA, PAPER, MODDED
+		# Variants: V0: Vanilla, P0: Paper, P1: Bedrock/Paper, M0: Fabric
+		typ = input("Specify a loader type. (vanilla | paper | fabric | bedrock-paper) >> ")
+		# Extra additions to specific versions (eg. bedrock-paper autoinstalls Geyser)
+		variant = 0
+		# Variant handler
+		if typ == "bedrock-paper": variant = 1
+		# Type handler
+		if typ == "paper": typ == "plugin"
+		if typ == "fabric": typ = "modded"
+		# Version List
 		vers = [ver for ver in jcont["versions"][typ]]
 		print("------")
 		print(vers)
 		ver = input("Specify a version from the list above >> ")
 		print("------")
-		fold = "server_"+ver+"_"+typ
+		ex = ""
+		if typ == "plugin":
+			if variant == 0: ex = "paper"
+			if variant == 1: ex = "bedrockpaper"
+		if typ == "modded":
+			if variant == 0: ex = "fabric"
+		fold = "server_"+ex+ver+"_"+typ
 		print("Creating "+fold+" folder...")
 		os.mkdir(fold)
 		print("Downloading server JAR file...")
@@ -49,6 +65,21 @@ else:
 		eula = open(fold+"\\eula.txt", "w")
 		eula.write("eula=true")
 		eula.close()
+		# Special folder
+		if typ != "vanilla":
+			if typ == "plugin":
+				print("Creating PLUGINS folder...")
+				os.mkdir("plugins")
+			if typ == "modded":
+				print("Creating MODS folder...")
+				os.mkdir("mods")
+		# Autofill special folder
+		if typ == "plugin" and variant == 1:
+			for mod in jcont["addons"]["bedrock-paper"].content:
+				fil = open(fold+f"\\plugins\\{mod.name}")
+				fil.write(requests.get(mod.url))
+				fil.close()
+		# Run helper file
 		print("Creating RUN batch file...")
 		runbat = open(fold+"\\run.bat", "w")
 		runbat.write("@echo off\njava -Xmx1G -Xms1G -jar server.jar nogui\npause")
